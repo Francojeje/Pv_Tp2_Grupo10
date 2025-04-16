84f0eaf5b7efe4eccecd9ebfa7c5abb457ad68d9
Francojeje <francoalmaraz711@gmail.com>
2025-04-16 20:31:05 -0300

    ejercicio13

--- Js/ejercicio13.js
+++ Js/ejercicio13.js
@@ -1,5 +1,8 @@
-
-function mostrararreglo(){
-
-
+function mostrararreglo(arreglo){
+    for(let i = 0 ; i < arreglo.length ; i++){
+        console.log ("Numero " + arreglo[i]);
+    }
 }
+let arreglo = [3,4,6,7,8];
+mostrararreglo(arreglo)
+window.alert(arreglo)

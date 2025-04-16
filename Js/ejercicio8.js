4ca887dce8d5234c6eeb38ebc9d6d10b8fec4107
Francojeje <francoalmaraz711@gmail.com>
2025-04-16 19:00:53 -0300

    ejercicio8

---
+++ Js/ejercicio8.js
@@ -0,0 +1,14 @@
+function calcularMayor (numero1, numero2){
+    if (numero1==numero2){
+        console.log("Son iguales")
+    }
+    else{
+        if (numero1>numero2){
+        console.log ("El primer numero es el mayor")
+        }
+           else{
+        console.log("El segundo numero es el mayor");
+        } 
+    }
+}
+calcularMayor(5,5);

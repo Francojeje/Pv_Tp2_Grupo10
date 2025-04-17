47e087d6354c4edb544b5fde15a7b1c60099c988
Francojeje <francoalmaraz711@gmail.com>
2025-04-17 11:03:54 -0300

    ejercicio18

---
+++ Js/ejercicio18.js
@@ -0,0 +1,5 @@
+document.getElementById("boton").addEventListener("click", cambiodecolor);
+function cambiodecolor (){
+    let parrafo = document.querySelector(".parrafo")
+    parrafo.style.color= "red"
+}

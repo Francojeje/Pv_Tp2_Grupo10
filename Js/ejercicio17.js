fc6dc5085b9c0c632cf4acc91997c020718d0fb3
Francojeje <francoalmaraz711@gmail.com>
2025-04-16 22:02:20 -0300

    Ejercicio17

---
+++ Js/ejercicio17.js
@@ -0,0 +1,12 @@
+function contarletra (cadena){
+    let long = cadena.length 
+    let cont = 0 
+    for(let i = 0 ; i < long ; i++){
+        if (cadena[i]== 'a' || cadena[i]== 'A'){
+            cont = cont + 1
+        }
+    }
+    return alert("Cantidad de 'A o a' en el string: " + cont)
+}
+let string = prompt("Ingrese un string")
+contarletra(string)

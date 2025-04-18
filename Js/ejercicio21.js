b940303ff76bf04401599cfd4187a902d60da51a
Francojeje <francoalmaraz711@gmail.com>
2025-04-18 12:39:17 -0300

    ejercicio23

--- Js/ejercicio21.js
+++ Js/ejercicio21.js
@@ -2,6 +2,6 @@
 let input = document.getElementById('cajadetexto')
 let parrafo = document.getElementById('textorepe')
 
-    input.addEventListener('input', () => {
+    input.addEventListener('input', function() {
       parrafo.textContent = input.value;
 });

47627bfb89c665ab10f3012bfaeff58936f3ba54
Francojeje <francoalmaraz711@gmail.com>
2025-04-18 02:11:05 -0300

    color black

--- Js/ejercicio22.js
+++ Js/ejercicio22.js
@@ -2,7 +2,7 @@ let input = document.getElementById('cajadetexto')
 let parrafo = document.getElementById('textorepe')
     input.addEventListener('input', function () {
       parrafo.textContent = input.value;
-      let colores = ["red","lightblue","green","pink","orange","purple","blue"]
+      let colores = ["black","red","lightblue","green","pink","orange","purple","blue"]
         const caracteres = input.value.length
         const indicecolor = Math.floor(caracteres / 20) % colores.length;
         parrafo.style.backgroundColor= colores[indicecolor] 

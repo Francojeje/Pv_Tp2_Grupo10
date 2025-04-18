2d5615f0cf27db82627cff3d0f5551c5b3399ef8
Francojeje <francoalmaraz711@gmail.com>
2025-04-18 13:06:24 -0300

    event

--- Js/ejercicio24.js
+++ Js/ejercicio24.js
@@ -1,7 +1,7 @@
 document.getElementById("boton").addEventListener("click", cambiodecolor);
 let body = document.getElementById("body")
-function cambiodecolor(e){
-    e.preventDefault();
+function cambiodecolor(event){
+    event.preventDefault();
     let colores = ["red","lightblue","green","pink","orange","purple","blue","brown","black","grey","lightgreen","yellow","beige"]
     const indicecolor = Math.floor(Math.random() * colores.length);;
     body.style.backgroundColor = colores[indicecolor]

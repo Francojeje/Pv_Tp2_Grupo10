796f907a27b01ddbf4c1b8461b94170ffd6092c2
Francojeje <francoalmaraz711@gmail.com>
2025-04-18 12:42:30 -0300

    mensaje en consola

--- Js/ejercicio23.js
+++ Js/ejercicio23.js
@@ -3,5 +3,6 @@ let parrafo = document.getElementById('textorepe')
 opcion.addEventListener("change",function(e){
         if (e.target.name === 'opcion') {
         parrafo.textContent = "Elegiste: "+  e.target.value;
+        console.log(parrafo.textContent)
     }
 })
